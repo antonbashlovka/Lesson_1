bcb5a9ba03d759f61e520d0350369bc7d98aa5eb
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-03 16:51:47 +0500

    added task_5 find out if the number is even

---
+++ task_6/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+    Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+    4 -> да
+    -3 -> нет
+    7 -> нет
+*/
+
+int a = 4, b = -3, c = 7;
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+if (b % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+if (c % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+
+
