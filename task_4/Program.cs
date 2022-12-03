2ce11ec0977ef91a6c44e0b08598926401cfe620
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-03 16:38:26 +0500

    added task_4 find max number

---
+++ task_4/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+    Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+    2, 3, 7 -> 7
+    44 5 78 -> 78
+    22 3 9 -> 22
+*/
+
+int a = 2, b = 3, c = 7;
+int max = a;
+
+if (b > max) {max = b;}
+if (c > max) {max = c;}
+
+Console.WriteLine("max = " + max);
+
+
+a = 4; 
+b = 5; 
+c = 78;
+max = a;
+
+if (b > max) {max = b;}
+if (c > max) {max = c;}
+
+Console.WriteLine("max = " + max);
+
+
+a = 22; 
+b = 3; 
+c = 9;
+max = a;
+
+if (b > max) {max = b;}
+if (c > max) {max = c;}
+
+Console.WriteLine("max = " + max);
