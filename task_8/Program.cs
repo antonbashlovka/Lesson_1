3bba00d06badc424711ec1d0e1e05586566a6b60
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-03 17:19:49 +0500

    added task_8 find even numbers

---
+++ task_8/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+    Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+    5 -> 2, 4
+    8 -> 2, 4, 6, 8
+*/
+
+int N = 5;
+int count = 1;
+string res = ""; 
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        res = res + count + ", ";
+    }
+    
+    count++;
+}
+
+Console.WriteLine(res.TrimEnd(new Char[] {',', ' '}));
